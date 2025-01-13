fafe3a9dfdec982868b13a79bb95c11f43dc277e
ANIRUDDH SHARMA <sharmaaniruddh811.com>
2025-01-13 22:14:50 +0530

    ..

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,13 +1,15 @@
 import express, { urlencoded } from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config({ path: "./env", })
 
 
 const app = express()
-app.use(cors(
-     origin = `${process.env.CORS_URL}`,
-     credentials = true,
-
+app.use(cors({
+     origin : `${process.env.CORS_URL}`,
+     credentials : true,
+}
 ))
 
 app.use(express.json({ limit: "16kb" })) //to send data into json formate 
