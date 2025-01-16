7ad2b6c9037587694b9c2ad9856e5406f619b054
ANIRUDDH SHARMA <sharmaaniruddh811@gmail.com>
2025-01-17 00:41:18 +0530

    creating commit to check profile graphs

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,13 +1,10 @@
-import  asyncHandler from "../utils/asyncHandler.js";
+import asyncHandler from "../utils/asyncHandler.js";
 
 
-const registerUser=asyncHandler(async(req,res)=>{
+const registerUser = asyncHandler(async (req, res) => {
      res.status(200).json({
-          message:"aniruddh sharma is here "
-          
-
-          
-})
+          message: "aniruddh sharma is here "
+     })
 })
 
 export default registerUser;
